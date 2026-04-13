d15f03b09ab813bf3b1c8cd71d38af79990df2ae
luoyi <luoyi58624@gmail.com>
2026-04-13 10:43:01 +0800

    ++

--- lib/runpoint_app_commons.dart
+++ lib/runpoint_app_commons.dart
@@ -4,3 +4,5 @@ export 'package:el_dart/el_dart.dart';
 export 'package:el_dart/ext.dart';
 export 'package:el_flutter/el_flutter.dart';
 export 'package:el_flutter/ext.dart';
+
+export 'src/utils/app_update/app_update_dialog.dart';
