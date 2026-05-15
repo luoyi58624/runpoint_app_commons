c7f2ce7585b2c6afd20e97a89c58f2d31ce1c99b
luoyi <luoyi58624@gmail.com>
2026-05-15 19:23:58 +0800

    chore: 更新依赖

--- lib/src/utils/app_update/apk_update.dart
+++ lib/src/utils/app_update/apk_update.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:android_package_installer/android_package_installer.dart';
-import 'package:el_flutter/ext.dart';
+import 'package:el_dart/el_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
